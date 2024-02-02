0174dacd6f0c37be7cd6f8c44e5a3da83cf4f03e
FzHars <aljawif1@gmail.com>
2024-02-03 03:04:06 +0700

    3

---
+++ Pengulangan/while.js
@@ -0,0 +1,21 @@
+// console.log("Hello World");
+// Infinite loop
+// while (true){
+//     console.log("hello world");
+// }
+
+// Menghentikan loop forever dengan user
+// var ulang = true;
+// while (ulang === true) {
+//   console.log("Hello world");
+//   ulang = confirm("Ulang ? ");
+// }
+
+// Menghentikan loop dengan program
+var nilaiAwal = 1;
+while (nilaiAwal <= 5) {
+  console.log("Hello world");
+  // nilaiAwal = nilaiAwal + 1;
+  // atau simpelnya
+  nilaiAwal++;
+}
---
+++ Pengulangan/while2.js
@@ -0,0 +1,5 @@
+var nilaiAwal = 1;
+while (nilaiAwal <= 7) {
+  console.log("Hello World ke " + nilaiAwal + "x");
+  nilaiAwal++;
+}
